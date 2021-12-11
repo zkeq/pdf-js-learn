5994cdb74be05e82cd3420856c6453c134f0439d
zkeq <62864752+zkeq@users.noreply.github.com>
2021-12-11 13:42:31 +0800

    relearn

--- web/viewer.js
+++ web/viewer.js
@@ -49,7 +49,7 @@ const defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: "compressed.tracemonkey-pldi-09.pdf",
+    value: "learnonly.pdf",
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
